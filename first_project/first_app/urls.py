d34ec25337c8bb3c613e16e412ea4342e8cc7305
ivan006 <ivan.copeland2015@gmail.com>
2020-09-03 10:41:04 +0200

    1

--- first_project/first_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from first_app import views
-app_name = "first_app"
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('register/', views.register, name='register'),
-    path('user_login/', views.user_login, name='user_login'),
-]
